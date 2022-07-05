c6057b9d8e229b75b433644106ad1ea284410b13
Vignesh E <evicky2002@gmail.com>
2022-07-06 00:09:45 +0530

    commit

---
+++ linked_list/LinkedList.java
@@ -0,0 +1,21 @@
+public class LinkedList {
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public void addFirst(String data) {
+        if (head == null) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+    }
+}
